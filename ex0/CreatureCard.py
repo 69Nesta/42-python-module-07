f1ec4c40919e7cafcd9ca414fd8eef2f285e1c45
69Nesta <petitromeo74@gmail.com>
2026-02-06 14:34:47 +0100

    Feat: card template ex0

---
+++ ex0/CreatureCard.py
@@ -0,0 +1,21 @@
+from .Card import Card
+
+
+class CreatureCard(Card):
+    def __init__(
+                self,
+                name: str,
+                cost: int,
+                rarity: str,
+                attack: int,
+                health: int
+            ) -> None:
+        super().__init__(name, cost, rarity)
+        self.attack = attack
+        self.health = health
+
+    def play(self, game_state: dict) -> dict:
+        return {}
+
+    def attack_target(self, target) -> dict:
+        return {}
